2124adc540fdd2426b3d86b636d122b2687050f3
Antonio Abadia <antonio.abadia@aimen.es>
2025-04-29 11:37:52 +0200

    asdasd

---
+++ 1-webminar.sh
@@ -0,0 +1,9 @@
+# https://technical-compliance-workshop-fe23a7.gitlab.io/gaia-x-ready-did/
+# Create keypair 
+# Genera el archivo private_key.pem
+openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
+# extract the public key (public_key.pem)
+openssl rsa -in private_key.pem -pubout -out public_key.pem
+
+# Step 4: Create a self-signed certificate using the private key
+openssl req -new -x509 -key private_key.pem -out cert.pem -days 365 -subj "/CN=example.com"
